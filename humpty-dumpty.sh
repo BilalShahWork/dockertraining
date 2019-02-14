5dc8afd53c663ea7e1931415a549a3dda96cebf2
Bilal Shah <bilal.shah@oracle.com>
2019-02-14 18:14:42 +0000

    my first file

---
+++ humpty-dumpty.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Acknowledgements: https://en.wikipedia.org/wiki/Humpty_Dumpty
+#
+echo "+------------------------------------------------+"
+echo `date`
+echo "+------------------------------------------------+"
+echo "|                                                |"
+echo "|  Humpty Dumpty sat on a wall                   |"
+echo "|                                                |"
+echo "|  Humpty Dumpty had a great fall                |"
+echo "|                                                |"
+echo "|  All the King's horses and all the king's men  |"
+echo "|                                                |"
+echo "|  Couldn't put Humpty Dumpty together again     |"
+echo "|                                                |"
+echo "+------------------------------------------------+"
